0f874b9e9376c003387600ec89ba2ad4f84e494d
osman <osmnndm@protonmail.com>
2020-06-21 21:14:27 +0200

    ikinci commit

--- package01/firstClass.java
+++ package01/firstClass.java
@@ -5,6 +5,7 @@ public class firstClass {
 	public static void main(String[] args) {
 		
 		System.out.println("ilk commit from local repository");
+		System.out.println("ikinci commit from local repository");
 		
 	}
 	
